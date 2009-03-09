44e40cb139ecf325f6a343fd0ddcaf6bc4c2b7b6
castano <castano@9a2b036c-014e-11de-8259-6defae454439>
2009-03-09 22:46:49 +0000

    Merge changes from internal repository.

---
+++ trunk/src/nvcore/AsyncFile.h
@@ -0,0 +1,17 @@
+// This code is in the public domain -- castano@gmail.com
+
+#ifndef NV_CORE_ASYNCFILE_H
+#define NV_CORE_ASYNCFILE_H
+
+#include <nvcore/Debug.h>
+
+#if NV_OS_LINUX
+#include <aio.h>
+#endif
+
+namespace nv
+{
+	
+} // nv namespace
+
+#endif // NV_IO_FILE_H
