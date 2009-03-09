2f30c7d6da39092e68a86dc756910be0cfe6c340
castano <castano@9a2b036c-014e-11de-8259-6defae454439>
2009-03-09 22:45:08 +0000

    Add spin waiter class.

---
+++ src/nvthreads/SpinWaiter.h
@@ -0,0 +1,23 @@
+// This code is in the public domain -- castano@gmail.com
+
+#ifndef NV_THREADS_SPINWAITER_H
+#define NV_THREADS_SPINWAITER_H
+
+#include <nvthreads/nvthreads.h>
+
+namespace nv
+{
+	// Spin wait helper class.
+	struct SpinWaiter
+	{
+		Waiter() : m_count(0) {}
+		
+		void backoff();
+		
+	private:
+		int m_count;
+	};
+	
+} // nv namespace
+
+#endif // NV_THREADS_THREADUTIL_H
